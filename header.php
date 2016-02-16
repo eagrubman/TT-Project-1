ff416a8cc90e69946b6e322c959922ae866c3172
eagrubman <grubman.ed@gmail.com>
2016-02-15 21:27:47 -0800

    Updates
    Updated - contact.html - for id name
    Removed Portfolio.html

--- header.php
+++
@@ -1,7 +0,0 @@
-<?php
-<header>
-      <img src="images/profile.jpg" alt="Eduard's Profile Picture" class="profile-image">
-      <h1 class="tag name">Eduard </h1>
-      <p class="tag location">Native Angeleno Living in the City of Angeles</p>
-    </header>
-?>
